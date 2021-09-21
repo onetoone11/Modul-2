8757f78fcf109189e3796486d2b238cbb893a2c8
MildusM <90381854+MildusM@users.noreply.github.com>
2021-09-21 09:34:38 +0200

    Update script.js

--- modul2/script.js
+++ modul2/script.js
@@ -1,7 +1,30 @@
 // Assets
 
+// let audio = new Audio("img/wii.mp3");
+// audio.play();
+
 let sprite = document.querySelector('#sprite');
 let backgroundMap = document.querySelector('#backgroundMap');
+
+
+let test = document.querySelector('.test');
+let test2 = document.querySelector('.test2');
+let test3 = document.querySelector('.test3');
+let test4 = document.querySelector('.test4');
+let btnWalkIn = document.querySelector('.btn-walkIn');
+
+let hide = document.querySelector('.hide');
+
+let show = document.querySelector('.show');
+
+let log = document.querySelector('.conversation-log');
+
+
+let lisa = document.querySelector('.lisa');
+let patrick = document.querySelector('.patrick');
+let bob = document.querySelector('.bob');
+let mandy = document.querySelector('.mandy');
+
 
 sprite.style.backgroundColor = "green";
 
@@ -21,6 +44,9 @@ let backgroundMapWidth = backgroundMap.offsetWidth;
 let backgroundMapPaddingTop = (screenHeight - backgroundMapHeight)/2;
 backgroundMap.style.paddingTop = parseInt(backgroundMapPaddingTop/2) + 'px';
 
+let logPaddingTop = (screenHeight - backgroundMapHeight)/2;
+log.style.paddingTop = parseInt(logPaddingTop/2) + 'px';
+
 // Position
 
 let spriteBottomInnerPositionOfMap = parseInt(backgroundMap.style.paddingTop);
@@ -28,6 +54,23 @@ let spriteTopInnerPositionOfMap = spriteBottomInnerPositionOfMap + backgroundMap
 
 let spriteLeftInnerPositionOfMap = (screenWidth - backgroundMapWidth)/2;
 let spriteRightInnerPositionOfMap = spriteLeftInnerPositionOfMap + backgroundMapWidth -10;
+
+    // Characters position
+
+patrick.style.top = parseInt(screenHeight*0.61) + 'px';
+patrick.style.left = parseInt(screenWidth*0.61) + 'px';
+
+
+    // Grinder Position
+
+let grinder = [screenHeight*0.44, screenHeight*0.565, screenWidth*0.46, screenWidth*0.52, "Grinder"];
+
+let house = [screenHeight*0.31, screenHeight*0.43, screenWidth*0.365, screenWidth*0.425, "House"];
+
+let fish = [screenHeight*0.56, screenHeight*0.69, screenWidth*0.63, screenWidth*0.69, "FishHouse"];
+
+let barn = [screenHeight*0.56, screenHeight*0.69, screenWidth*0.32, screenWidth*0.38, "Barn"]; 
+
 
 
 // Sprite, Position
@@ -43,6 +86,12 @@ window.addEventListener('load', () => {
 let spriteSpeed = 5;
 
 window.addEventListener('keydown', (event) => {
+    buttons();
+
+    gamePosition((grinder[0]),(grinder[1]),(grinder[2]), (grinder[3]), (grinder[4]));
+    gamePosition((house[0]), (house[1]), (house[2]), (house[3]), (house[4]));
+    gamePosition((fish[0]), (fish[1]), (fish[2]), (fish[3]), (fish[4]));
+    gamePosition((barn[0]), (barn[1]), (barn[2]), (barn[3]), (barn[4]));
 
     if(event.key == 'ArrowDown' && parseInt(sprite.style.top) < spriteTopInnerPositionOfMap){
         sprite.style.top = parseInt(sprite.style.top) + spriteSpeed + 'px';
@@ -57,6 +106,114 @@ window.addEventListener('keydown', (event) => {
         sprite.style.left = parseInt(sprite.style.left) - spriteSpeed + 'px';
     }
 });
+
+
+btnWalkIn.style.top = screenHeight*0.09 + 'px';
+btnWalkIn.style.left = screenWidth*0.31 + 'px';
+
+
+
+let buttonsTrueFalse = false;
+buttons();
+
+function gamePosition(h1, h2, b1, b2, building){
+
+    h1 = Math.floor(h1);
+    h2 = Math.floor(h2);
+    b1 = Math.floor(b1);
+    b2 = Math.floor(b2);
+
+    
+
+    if(parseInt(sprite.style.left) > b1 && parseInt(sprite.style.left) < b2 && parseInt(sprite.style.top) > h1 && parseInt(sprite.style.top) < h2){
+        buttonsTrueFalse = true;
+        btnWalkIn.style.display = "block";
+        
+        btnWalkIn.id = `${building}`;
+    }
+    else {
+        buttonsTrueFalse = false;
+    }
+
+}
+
+function buttons(){
+
+    if(buttonsTrueFalse == true){
+        btnWalkIn.style.display = "block";
+    }
+    else if(buttonsTrueFalse == false){
+        btnWalkIn.style.display = "none";
+    }
+
+}
+
+function hideContent(){
+    hide.style.display = "none";
+    show.style.display = "block";
+
+
+    let a = btnWalkIn.id;
+
+    conversationLog(a);
+
+    btnWalkIn.id = '';
+}
+
+
+function conversationLog(place){
+
+    if(place == grinderContent.building){
+        
+        patrick.style.display = "block";
+    }
+    
+}
+
+
+const grinderContent = {
+
+    building: 'Grinder',
+    workerName: 'Patrick',
+    workerImg: 'img/patrickGrinder.png',
+    id: 1
+
+};
+
+const houseContent = {
+    
+    building: 'House',
+    workerName: 'Lisa',
+    workerImg: 'img/lisaHouse.png',
+    id: 2
+
+}
+
+const fishHouseContent = {
+
+    building: 'FishHouse',
+    workerName: 'Bob',
+    workerImg: 'img/bobFishHouse.png',
+    id: 3
+
+}
+
+const barnContent = {
+
+    building: 'Barn',
+    workerName: 'Mandy',
+    workerImg: 'img/mandyBarn.png',
+    id: 4
+
+}
+
+
+
+
+
+
+
+
 
 
 
