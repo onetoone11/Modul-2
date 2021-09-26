9e947941f4380c875c39b5cb6e071763e5c2f4f8
MildusM <90381854+MildusM@users.noreply.github.com>
2021-09-26 21:34:51 +0200

    Update script.js

--- modul2/script.js
+++ modul2/script.js
@@ -1,16 +1,15 @@
 // Assets
 
-// let audio = new Audio("img/wii.mp3");
-// audio.play();
-
 let health;
 
 let money;
 
 let earnedkey;
 
+// LocalStorage
+
 if(localStorage.health == undefined){
-    health = 1000;
+    health = 100;
 }
 else{
     health = Number(localStorage.health);
@@ -31,7 +30,7 @@ else{
 }
 
 
-
+// playerBarIcons
 
 document.querySelector('.pBolt').innerHTML = '<i class="fas fa-bolt bolt playerBarIcon"></i>   ' + health; 
 
@@ -61,6 +60,7 @@ let patrick = document.querySelector('.patrick');
 let bob = document.querySelector('.bob');
 let mandy = document.querySelector('.mandy');
 let player = document.querySelector('.player');
+let monster = document.querySelector('.monster');
 
 let pPlayer = document.querySelector('.pPlayer');
 let pWorker = document.querySelector('.pWorker');
@@ -98,12 +98,60 @@ let questInformation = document.querySelector('.questInformation');
 
 
 
+let gameOver = document.querySelector('.gameOver');
+
+let winner = document.querySelector('.winner');
+
+let restart = document.querySelector('.restart');
+
+let gameEndText = document.querySelector('.gameEndText');
+
+
+// Chest
+
+let chestTitle = document.querySelector('.chestTitle');
+
+let chestSuccessP = document.querySelector('.chestSuccessP');
+
+let chestFailureP = document.querySelector('.chestFailureP');
+
+let chestFailureBtn = document.querySelector('.chestFailureBtn');
+
+let chestSuccessBtnOpen = document.querySelector('.chestSuccessBtnOpen');
+
+let chestSuccessBtnNo = document.querySelector('.chestSuccessBtnNo');
+
+let chestSuccessBtn = document.querySelector('.chestSuccessBtn');
+
+let lockedChest = document.querySelector('.lockedChest');
+
+let unlockedChest = document.querySelector('.unlockedChest');
+
+let coin = document.querySelector('.coinAnimation');
+
+let chestDiv = document.querySelector('.chest');
+
+
+// Monster
+
+let monsterDiv = document.querySelector('.monsterDiv');
+
+let monsterTitle = document.querySelector('.monsterTitle');
+
+let monsterP = document.querySelector('.monsterP');
+
+let monsterBtn = document.querySelector('.monsterBtn');
+
+let monsterFightBtn = document.querySelector('.monsterFightBtn');
+
+let monsterGiveUp = document.querySelector('.monsterGiveUp');
 
 
 
 
 
-sprite.style.backgroundColor = "green";
+
+// sprite.style.backgroundColor = "green";
 
 const grinderContent = {
 
@@ -149,6 +197,14 @@ const playerContent = {
 
 }
 
+const monsterContent = {
+
+    playerName: 'Monster',
+    playerImg: 'img/monster.png',
+    id: 6
+
+}
+
 
 // Size
 
@@ -157,6 +213,14 @@ const h = window.innerHeight;
 
 const screenWidth = window.screen.width;
 const screenHeight = window.screen.height;
+// function varChange(){
+//     screenHeight = window.screen.height;
+//     screenWidth = window.screen.width;
+//     sprite.style.top = screenHeight*0.62 + 'px';
+//     sprite.style.left = screenWidth*0.485 + 'px';
+// }
+
+// window.onresize = varChange;
 
 let backgroundMapHeight = backgroundMap.offsetHeight;
 let backgroundMapWidth = backgroundMap.offsetWidth;
@@ -197,6 +261,9 @@ mandy.style.left = parseInt(screenWidth*0.61) + 'px';
 player.style.top = parseInt(screenHeight*0.61) + 'px';
 player.style.left = parseInt(screenWidth*0.29) + 'px';
 
+monster.style.top = parseInt(screenHeight*0.61) + 'px';
+monster.style.left = parseInt(screenWidth*0.61) + 'px';
+
     // Character Text Position
 
 pPlayer.style.top = parseInt(screenHeight*0.57) + 'px';
@@ -227,6 +294,55 @@ questRandomButtons.style.top = parseInt(screenHeight*0.5) + 'px';
 questInformation.style.top = parseInt(screenHeight*0.1) + 'px'; 
 
 
+gameOver.style.top = parseInt(screenHeight*0.15) + 'px';
+
+winner.style.top = parseInt(screenHeight*0.15) + 'px';
+
+restart.style.top = parseInt(screenHeight*0.6) + 'px';
+
+gameEndText.style.top = parseInt(screenHeight*0.37) + 'px';
+
+
+
+
+
+chestTitle.style.top = parseInt(screenHeight*0.10) + 'px';
+
+chestSuccessP.style.top = parseInt(screenHeight*0.25) + 'px';
+
+chestFailureP.style.top = parseInt(screenHeight*0.25) + 'px';
+
+chestSuccessBtn.style.top = parseInt(screenHeight*0.4) + 'px';
+
+chestFailureBtn.style.top = parseInt(screenHeight*0.4) + 'px';
+
+unlockedChest.style.top = parseInt(screenHeight*0.53) + 'px';
+
+lockedChest.style.top = parseInt(screenHeight*0.53) + 'px';
+
+
+
+
+// Monster
+
+monsterTitle.style.top = parseInt(screenHeight*0.1) + 'px';
+
+monsterP.style.top = parseInt(screenHeight*0.3) + 'px';
+
+monsterBtn.style.top = parseInt(screenHeight*0.5) + 'px';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 btnWalkIn.style.top = screenHeight*0.09 + 'px';
 btnWalkIn.style.left = screenWidth*0.31 + 'px';
@@ -235,7 +351,7 @@ btnWalkIn.style.left = screenWidth*0.31 + 'px';
 // questRandom.style.top = parseInt(screenHeight*0.10) + 'px';
 
 
-    // Grinder Position
+    // Buildings Position
 
 let grinder = [screenHeight*0.44, screenHeight*0.565, screenWidth*0.46, screenWidth*0.52, "grinder"];
 
@@ -250,16 +366,24 @@ let barn = [screenHeight*0.56, screenHeight*0.69, screenWidth*0.32, screenWidth*
 
 let grinderQuests = ['', ''];
 
-grinderQuests[0] = ['Keyquest', 'Someone dropped all the keys downstairs, can you please help me find them all?'];
-grinderQuests[1] = ['Treequest', 'A tree just fell, can you help me move it?'];
+grinderQuests[0] = ['Keyquest', 'Someone dropped all the keys downstairs, can you please help me find them all, by complete a quest.'];
+grinderQuests[1] = ['Treequest', 'A tree just fell, can you help me move it, by complete a quest?'];
 
 
-// Sprite, Position
+
+
+let award = ['key', 'money', 'energy'];
+
+let randomAward = Math.floor(Math.random()*3);
+
+let randomAmount = Math.floor(Math.random()*5 + 1);
+
+// window load
 
 window.addEventListener('load', () => {
     sprite.style.position = 'absolute';
-    sprite.style.top = screenHeight*0.62 + 'px';
-    sprite.style.left = screenWidth*0.485 + 'px';
+    sprite.style.top = screenHeight*0.61 + 'px';
+    sprite.style.left = screenWidth*0.48 + 'px';
     log.style.display = 'none';
 });
 
@@ -287,7 +411,7 @@ window.addEventListener('keydown', (event) => {
     if(event.key == 'ArrowLeft' && parseInt(sprite.style.left) > spriteLeftInnerPositionOfMap){
         sprite.style.left = parseInt(sprite.style.left) - spriteSpeed + 'px';
     }
-    if(health < 20){
+    if(health < 10){
         exhausted();
     }
     if(money < 5){
@@ -300,7 +424,7 @@ window.addEventListener('keydown', (event) => {
 
 window.addEventListener('click', (event) => {
     
-    if(health < 20){
+    if(health < 10){
         exhausted();
     }
     if(money < 5){
@@ -443,9 +567,19 @@ function conversationLog(place){
 
 
 
-function acceptQuest(healthVal, moneyVal){
-    if(health <= 0 || health - healthVal < 0 || money <= 0 || money - moneyVal < 0){
-        alert("Du är för trött!");
+function acceptQuest(healthVal){
+
+    let randomChestMonster = Math.floor(Math.random()*15);
+
+    if(randomChestMonster == 1){
+        // Chest
+        chest();
+
+    }
+    else if(randomChestMonster == 0){
+       // monster
+       monsterI();
+
     }
     else{
         health -= healthVal;
@@ -461,17 +595,98 @@ function acceptQuest(healthVal, moneyVal){
 
         log.style.display = 'block';
 
-        
-
         questInformation.style.display = 'block';
 
+        // Chest none
+        unlockedChest.style.display = 'none';
+        lockedChest.style.display = 'none';
+        coin.style.display = 'none';
+        chestFailureBtn.style.display = 'none';
+        chestSuccessBtnOpen.style.display = 'none';
+        chestSuccessBtnNo.style.display = 'none';
+        chestSuccessP.style.display = 'none';
+        chestFailureP.style.display = 'none';
+        chestTitle.style.display = 'none';
 
+        
 
 
     }
     
 }
 
+
+function chest(){
+    log.style.display = 'block';
+    show.style.display = 'none';
+
+    if(earnedkey == 0){
+        chestTitle.style.display = 'block';
+        chestFailureP.style.display = 'block';
+        chestFailureBtn.style.display = 'block';
+        lockedChest.style.display = 'block';
+    }
+    else if (earnedkey > 0){
+        chestTitle.style.display = 'block';
+        chestSuccessP.style.display = 'block';
+        chestSuccessP.innerHTML = 'Congrats! You found a chest! Do you want to open it?';
+        chestSuccessBtn.style.display = 'block';
+        chestSuccessBtnOpen.style.display = 'inline';
+        chestSuccessBtnNo.style.display = 'inline';
+        lockedChest.style.display = 'block';
+        unlockedChest.style.display = 'none';
+    }
+
+}
+
+function openChest(){
+    earnedkey -= 1;
+    localStorage.setItem('earnedkey', earnedkey);
+
+    document.querySelector('.pKey').innerHTML = `<i class="fas fa-key key playerBarIcon"></i>   ${earnedkey}`;
+
+    let awardAmount = 10 * randomAmount;
+    money += awardAmount;
+    localStorage.setItem('money', money);
+    document.querySelector('.pCoins').innerHTML = `<i class="fas fa-coins coins playerBarIcon"></i>   ${money}`;
+
+    chestSuccessP.innerHTML = `Congrats! You found ${awardAmount} <i style="padding: 0;" class="fas fa-coins coins playerBarIcon"></i> in this chest!`;
+    lockedChest.style.display = 'none';
+    unlockedChest.style.display = 'block';
+    chestSuccessBtn.style.display = 'none';
+    chestFailureBtn.style.display = 'block';
+    coin.style.display = 'block';
+
+}
+
+// else if(place == barnContent.building){
+
+//     mandy.style.display = 'block';
+//     player.style.display = 'block';
+//     pWorker.innerHTML = barnContent.workerName;
+//     pPlayer.innerHTML = playerContent.playerName;
+//     placeH1.innerHTML = barnContent.building.toUpperCase();
+
+//     playerBar.style.backgroundColor = 'transparent';
+
+// }
+
+function monsterI(){
+
+    log.style.display = 'block';
+    show.style.display = 'none';
+
+    player.style.display = 'block';
+    monster.style.display = 'block';
+
+    monsterTitle.style.display = 'block';
+    monsterP.style.display = 'block';
+    monsterP.innerHTML = 'Oh no! You met a monster! Do you want to fight or run away?'
+    monsterBtn.style.display = 'block';
+    monsterFightBtn.style.display = 'inline';
+    monsterGiveUp.style.display = 'inline';
+
+}
 
 
 function takeMoney(price){
@@ -499,30 +714,66 @@ function useKey(keys){
 }
 
 
+function restartGame(){
+    location.reload();
+    localStorage.clear();
+}
+
 function exhausted(){
-    alert("YOU ARE EXHAUSTED! Soon you will probably die... ")
+    // alert("YOU ARE EXHAUSTED!");
+
+    log.style.display = 'block';
+    hide.style.display = 'none';
+    playerBar.style.display = 'none';
+    gameOver.style.display = 'block';
+    restart.style.display = 'block';
+    gameEndText.style.display = 'block';
+    questInformation.style.display = 'none';
+    chestDiv.style.display = 'none';
+
+    gameEndText.innerHTML = `You are exhausted! You need to rest. During your journey you earned 
+    ${money} coins and ${earnedkey} keys`;
+
 }
 
 function broke(){
-    alert("YOU ARE BROKE!");
+    // alert("YOU ARE BROKE!");
+
+    log.style.display = 'block';
+    hide.style.display = 'none';
+    playerBar.style.display = 'none';
+    gameOver.style.display = 'block';
+    restart.style.display = 'block';
+    gameEndText.style.display = 'block';
+    questInformation.style.display = 'none';
+    chestDiv.style.display = 'none';
+
+    gameEndText.innerHTML = `You are broke! During your journey you earned 
+    ${earnedkey} keys and your health was ${health} hp`;
+
 }
 
 function rich(){
-    alert("YOU ARE RICH");
+    // alert("YOU ARE RICH");
+
+    log.style.display = 'block';
+    hide.style.display = 'none';
+    playerBar.style.display = 'none';
+    winner.style.display = 'block';
+    restart.style.display = 'block';
+    gameEndText.style.display = 'block';
+    questInformation.style.display = 'none';
+    chestDiv.style.display = 'none';
+
+    gameEndText.innerHTML = `Yay! You earned so much money that you now considers to be rich! During your 
+    journey, you earned ${earnedkey} keys and your health was ${health} hp`;
 }
 
 
 
-// -----------------------------
 
 
 
-let award = ['key', 'money', 'energy'];
-
-
-let randomAward = Math.floor(Math.random()*3);
-
-let randomAmount = Math.floor(Math.random()*5 + 1);
 
 
 // QuestRandom
@@ -538,45 +789,35 @@ function questRandomButton(buttonId){
         if(buttonId == randomNumber){
             if(randomAward == 0){
                 earnedkey += 1;
-
                 localStorage.setItem('earnedkey', earnedkey);
 
                 document.querySelector('.pKey').innerHTML = `<i class="fas fa-key key playerBarIcon"></i>   ${earnedkey}`;
                 alert('Congrats! Your award is a ' + award[randomAward]);
-                log.style.display = 'none';
+                
                 questRandomContentRestart();
-                questRandom.style.display = 'none';
-                hide.style.display = 'block';
-
-                playerBar.style.backgroundColor = '#384d00';
+                 
             }
             else if(randomAward == 1){
                 let awardAmount = 10 * randomAmount;
                 money += awardAmount;
                 localStorage.setItem('money', money);
-                document.querySelector('.pCoins').innerHTML = `<i class="fas fa-coins coins playerBarIcon"></i>   ${money}`;
 
+                document.querySelector('.pCoins').innerHTML = `<i class="fas fa-coins coins playerBarIcon"></i>   ${money}`;
                 alert('Congrats! Your award is ' + awardAmount + ' ' + award[randomAward]);
 
-                log.style.display = 'none';
-                questRandom.style.display = 'none';
-                hide.style.display = 'block';
-
-                playerBar.style.backgroundColor = '#384d00';
+                questRandomContentRestart();
+                
             }
             else if(randomAward == 2){
                 let awardAmount = 10 * randomAmount;
                 health += awardAmount;
-
                 localStorage.setItem('health', health);
+
                 document.querySelector('.pBolt').innerHTML = `<i class="fas fa-bolt bolt playerBarIcon"></i>   ${health}`;
                 alert('Congrats! Your award is ' + awardAmount + ' ' + award[randomAward]);
 
-                log.style.display = 'none';
-                questRandom.style.display = 'none';
-                hide.style.display = 'block';
-
-                playerBar.style.backgroundColor = '#384d00';
+                questRandomContentRestart();
+  
             }
         }
         else{
@@ -584,8 +825,9 @@ function questRandomButton(buttonId){
             attempts.innerHTML = attempts.innerHTML-1;
         }
     }
-    else if(attempts.innerHTML == 1){
-        alert('hej');
+    else if(attempts.innerHTML < 1 && buttonId != randomNumber){
+        alert('Quest is over! You did not win this time. Better luck next time!');
+        questRandomContentRestart();
     }
 
 }
@@ -620,6 +862,12 @@ function questRandomContentRestart(){
     document.querySelector('#slumpen8').style.display = 'none';
     document.querySelector('#slumpen9').style.display = 'none';
     document.querySelector('#slumpen10').style.display = 'none';
+
+    playerBar.style.backgroundColor = '#384d00';
+
+    log.style.display = 'none';
+    questRandom.style.display = 'none';
+    hide.style.display = 'block';
 }
 
 function questRandomContentShow(){
